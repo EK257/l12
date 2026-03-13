c3e399a6e814c7b1b8f1b1e8533541ea5b17ff23
EK257 <klime777@ya.ru>
2026-03-13 12:08:26 +0300

    Update Form1.cs

--- l12/Form1.cs
+++ l12/Form1.cs
@@ -9,19 +9,11 @@ namespace l12
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String title = textBox1.Text.Trim();
-            String author = textBox2.Text.Trim();
-            int year = (int)numericUpDown1.Value;
-
-            Book myBook = new Book(title, author, year);
-
+            Book myBook = new Book("Война и мир", "Лев Толстой", 1869);
             label4.Text = myBook.getInfo();
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
             Book unknownBook = new Book();
-            label4.Text = unknownBook.getInfo();
+            label4.Text += "\n" + unknownBook.getInfo();
         }
     }
 }
